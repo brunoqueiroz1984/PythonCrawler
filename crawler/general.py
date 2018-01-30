12f06235259ad677b890d1de2bd9bbd552d93107
brunoqueiroz1984 <bruno.limapaulista@gmail.com>
2018-01-30 17:33:56 -0200

    commit inicial

---
+++ crawler/general.py
@@ -0,0 +1,48 @@
+'''
+Created on 30 de jan de 2018
+
+@author: Bruno Queiroz
+'''
+import os
+from _io import open
+
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print("Criando projeto " + directory)
+        os.makedirs(directory)
+
+def create_data_files(project_name, base_url):
+    queue = project_name + "/queue.txt"
+    crawled = project_name + "/crawled.txt"
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, "")
+        
+
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
+    
+def append_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data + '\n')
+        
+def delete_file_content(path):
+    with open(path, 'w'):
+        pass
+
+
+def file_to_set(file_name):
+    results = set()
+    with open(file_name, 'r') as f:
+        for line in f:
+            results.add(line.replace('\n', ''))
+    return results
+
+def set_to_file(links, file):
+    delete_file_content(file)
+    for link in sorted(links):
+        append_to_file(file, link)
+    
